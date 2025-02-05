93ea21ed39518ba8375d3f6f316e4c0a5e5f27b1
Christopher Schuchardt <cschuchardt88@gmail.com>
2025-02-05 12:34:46 -0500

    start

--- src/Neo.CLI/Program.cs
+++ src/Neo.CLI/Program.cs
@@ -15,8 +15,15 @@ namespace Neo
 {
     static class Program
     {
+        private static readonly string s_blockHex =
+            "000000000000000000000000000000000000000000000000000000000000000000000000" +
+            "6c23be5d32679baa9c5c2aa0d329fd2a2441d7875d0f34d42f58f70428fbbbb9493ed0e58f01" +
+            "00000000000000000000000000000000000000000000000000000000000000000000000" +
+            "10001110100000000000000000000000000000000000000000000000000010000000000" +
+            "00000000000000000000000000000001000112010000";
         static void Main(string[] args)
         {
+            byte[] j = [2, 3, 4];
             var mainService = new MainService();
             mainService.Run(args);
         }
